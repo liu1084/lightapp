4681c3501a352a49a3ec123365d14ea30d8dd0e0
jun.liu.neu <jun.liu.neu@neusoft.com>
2016-07-11 15:40:30 +0800

    update

--- src/app/add-project.js
+++ src/app/add-project.js
@@ -2,7 +2,6 @@
  * Created by jim.liu on 2016-07-09.
  */
 $(function () {
-    neu.projectCreate.init();
     toastr.options = {
         closeMethod: 'fadeOut',
         closeDuration: '200',
@@ -11,108 +10,88 @@ $(function () {
         positionClass: 'toast-top-full-width',
         hideDuration: 500
     };
+
+    var neu = new Neu();
+    neu.init();
 });
 
-var neu = window.neu || {};
-var contextPath = 'http://10.4.45.46:8080/workflow';
-neu.projectCreate = {
+var contextPath = '/workflow';
+var Neu = function(){
+    this.selectedProducts = [];
+    this.products = [];
+};
+Neu.prototype = {
     init: function () {
-        this.bindAddItem();
-        this.bindStatus();
-        this.bindMinus();
-        this.bindSave();
+        this.save();
         this.bind();
-        this.initContent();
     },
-
     bind: function () {
         var _this = this;
         $('.i-checkbox').off('click').on('click', function () {
             $(this).toggleClass('i-checkbox-checked');
+            var productName = $(this).siblings('.description').text();
+            if ($(this).hasClass('i-checkbox-checked')){
+                _this.selectedProducts.push(productName);
+            }else {
+                _this.selectedProducts = $.grep(_this.selectedProducts, function(value){
+                    return productName !== value;
+                })
+            }
+            console.log(_this.selectedProducts);
         });
         $('.icon-nextui-icon-add').on('click', function () {
             //window.location.href = "add-project-detail.html";
-            $('#myModal').modal({
-                backdrop: 'static',
-                show: true,
-                keyboard: false
-            });
+            _this.getProducts();
+            console.log(_this.selectedProducts);
+            $('#myModal')
+                .modal({
+                    backdrop: 'static',
+                    show: true,
+                    keyboard: false
+                })
+                .on('show.bs.modal', function(e){
+                    _this.selectedProductsOnModal();
+                })
+                .on('hidden.bs.modal', function(){
+                    _this.selectedProducts = $.unique($.merge(_this.selectedProducts, _this.getNewSelectedProducts()));
+                    _this.renderSelectedProducts();
+                });
         });
 
         $('.icon-nextui-icon-delete').on('click', function () {
+            var form = $('.products form');
+            if ($(this).attr('show-select') === "true"){
+                form.find('.item').find('.avatar').prev('span').remove();
+                $(this).attr('show-select', false);
+                return false;
+            }
             var html = '<span class="i-checkbox i-checkbox-unchecked">' +
                 '<span class="icon-nextui-icon-check"></span>' +
                 '</span>';
-            $('.products form').find('.item').find('span.i-checkbox').remove();
-            $('.products form').find('.item').find('.avatar').before(html);
+            $(this).attr('show-select', true);
+
+            form.find('.item').find('span.i-checkbox').remove();
+            form.find('.item').find('.avatar').before(html);
             $('.i-checkbox').off('click').on('click', function () {
                 $(this).toggleClass('i-checkbox-checked');
             });
         });
 
         $('.delete').on('click', function () {
-            var line = $('.products form').find('.item').find('span.i-checkbox-checked');
-            if (line.length > 0) {
-                var item = $(line).parent('.item').remove();
-            }
+            var lines = $('.products form').find('.item').find('span.i-checkbox-checked');
+            $.each(lines, function(i, line){
+                var productName = $(line).parent('.item').children('.description').text();
+                _this.selectedProducts = $.grep(_this.selectedProducts, function(value){
+                    return value != productName;
+                });
+
+                $(line).parent('.item').remove();
+            });
+            console.log(_this.selectedProducts);
         });
 
     },
-
-    initContent: function () {
-        var params = window.location.search;
-        if (params.indexOf("id") >= 0) {
-            var id = params.split("=")[1];
-            if (id != null) {
-                document.title = "项目更新";
-                $.ajax({
-                    type: 'POST',
-                    async: false,
-                    dataType: 'json',
-                    data: {"id": id},
-                    url: contextPath + "/workflow/getUniqueSaleProject.action",
-                    success: function (data) {
-                        $("#id").val(data.id);
-                        $("#name").val(data.name);
-                        $("#custName").val(data.custName);
-                        $("#status").val(data.status);
-                        $("#signDate").val(data.signDate);
-                        $("#projectMoney").val(data.projectMoney);
-                        $("#remark").html(data.remark);
-                        if ($("#remark").attr("placeholder") == "项目信息补充说明" && $("#remark").html() == "" || $("#remark").html() == "项目信息补充说明") {
-                            $("#remark").html("项目信息补充说明");
-                            $("#remark").addClass("placeholder");
-                        } else {
-                            $("#remark").removeClass("placeholder");
-                        }
-                        document.title = "项目更新";
-                        var html = "";
-                        if (data.product != "") {
-                            var product = data.product.split("#");
-                            for (var i = 0; i < product.length; i++) {
-                                var html = html + "<div class=\"proItem minus\">" + product[i] + "</div>";
-                            }
-                        }
-                        $(".item0").find(".addItem").before(html);
-                    },
-                    error: function (data) {
-                        $('.weui_toast_content').html("获取数据失败！");
-                        $('#loadingToast').show();
-                        setTimeout(function () {
-                            $('#loadingToast').hide();
-                        }, 1000);
-                    }
-                })
-            }
-        }
-    },
-    getQueryString: function (name) {
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-        var r = window.location.search.substr(1).match(reg);
-        if (r != null) return unescape(r[2]);
-        return null;
-    },
-    bindSave: function () {
+    save: function () {
         var _this = this;
         $(".apply-for").click(function () {
             var id = $("#id").val();
@@ -121,14 +100,14 @@ neu.projectCreate = {
             var status = $("#status").val();
             var signDate = $("#signDate").val();
             var projectMoney = $("#projectMoney").val();
-            var product = _this.getProduct();
+            var product = _this.selectedProducts.join('#');
             var remark = $("#remark").html();
             if (name == null || name == "") {
                 toastr.warning('', '项目名不能为空');
                 return false;
             }
             if (custName == null || custName == "") {
-                toastr.warning('', '客户名不能为空')
+                toastr.warning('', '客户名不能为空');
                 return false;
             }
             $.ajax({
@@ -148,88 +127,52 @@ neu.projectCreate = {
             });
         });
     },
-    bindMinus: function () {
-        $(".item0").on('click', '.minus', function () {
-            $(this).remove();
-        });
-    },
-    bindStatus: function () {
-        $('.container').on('click', '.bt_iptText', function () {
-            var _bt = baidu.template;
-            $("#weui_actionsheet").html(_bt("statusTemplate", {}));
-            var mask = $('#mask');
-            var weuiActionsheet = $('#weui_actionsheet');
-            weuiActionsheet.addClass('weui_actionsheet_toggle');
-            mask.show().addClass('weui_fade_toggle').one('click', function () {
-                hideActionSheet(weuiActionsheet, mask);
-            });
-            $('#actionsheet_cancel').one('click', function () {
-                hideActionSheet(weuiActionsheet, mask);
-            });
-            $('#actionSheet_wrap').one('click', '.bt_cell', function () {
-                $("#status").val($(this).html());
-                hideActionSheet(weuiActionsheet, mask);
-            })
-            weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
-            function hideActionSheet(weuiActionsheet, mask) {
-                weuiActionsheet.removeClass('weui_actionsheet_toggle');
-                mask.removeClass('weui_fade_toggle');
-                weuiActionsheet.on('transitionend', function () {
-                    mask.hide();
-                }).on('webkitTransitionEnd', function () {
-                })
-            }
-
-        });
-    },
-    getProduct: function () {
-        var status = "";
-        $(".item0 .minus").each(function () {
-            status = status + "#" + $(this).html();
-        })
-        return status.length == 0 ? status : status.substring(1, status.length);
-    },
-    bindAddItem: function () {
+    getProducts: function () {
         var _this = this;
-        $('.container').on('click', '.bt_addItem', function () {
-            var _bt = baidu.template;
-            var status = "";
-            $("#weui_actionsheet").html(_bt("productTemplate", {"status": _this.getProduct()}));
-            var mask = $('#mask');
-            var weuiActionsheet = $('#weui_actionsheet');
-            weuiActionsheet.addClass('weui_actionsheet_toggle');
-            mask.show().addClass('weui_fade_toggle').one('click', function () {
-                hideActionSheet(weuiActionsheet, mask);
-            });
-            $('#actionsheet_cancel').one('click', function () {
-                hideActionSheet(weuiActionsheet, mask);
-            });
-            $('#actionSheet_wrap').on('click', '.bt_cell_item', function () {
-                if ($(this).hasClass("choose")) {
-                    $(this).removeClass("choose");
-                    var pro = $(this).html();
-                    $(".item0").find(".minus").each(function () {
-                        if ($(this).html() == pro) {
-                            $(this).remove();
-                        }
-                    })
-                } else {
-                    $(".item0").find(".addItem").before("<div class='proItem minus'>" + $(this).html() + "</div>");
-                    $(this).addClass("choose");
-                }
-            })
-            weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
-            function hideActionSheet(weuiActionsheet, mask) {
-                $('#actionSheet_wrap').off();
-                weuiActionsheet.removeClass('weui_actionsheet_toggle');
-                mask.removeClass('weui_fade_toggle');
-                weuiActionsheet.on('transitionend', function () {
-                    mask.hide();
-                }).on('webkitTransitionEnd', function () {
-                    mask.hide();
-                })
-            }
-        })
-    }
+        $(".products .list-body ul > .item").each(function(){
+            _this.selectedProducts.push($(this).find('.description').html());
+        });
 
+        $.unique(_this.selectedProducts);
+    },
+    getNewSelectedProducts: function(){
+        var selectedProducts = [];
+        var products = $('.select-products .item .i-checkbox-checked');
+        $.each(products, function(i, p){
+            var thiz = this;
+            var productName = $(thiz).siblings('.description').text();
+            selectedProducts.push(productName);
+        });
+        return selectedProducts;
+    },
+    renderSelectedProducts: function(){
+        var _this = this;
+        console.log(_this.selectedProducts);
+        var html = '';
+        $.each(_this.selectedProducts, function(i, p){
+            var letter = p.substr(0, 1);
+            html +='<li class="item">';
+            html +='    <span class="avatar avatar-' + letter.toLowerCase() + '">' + letter + '</span>';
+            html +='    <span class="description">' + p + '</span>';
+            html +='    <span class="number">';
+            html +='        <input value="" maxlength="10" placeholder="输入金额" type="text"/>';
+            html +='    </span>';
+            html +='</li>';
+        });
+
+        $('.products .list-body ul').html(html);
+        $('.icon-nextui-icon-delete').attr('show-select', false);
+    },
+    selectedProductsOnModal: function(){
+        var _this = this;
+        var products = $('.select-products .item .i-checkbox');
+        $.each(products, function(i, p){
+            var productName = $(this).siblings('.description').text();
+            if ($.inArray(productName, _this.selectedProducts) > -1){
+                $(this).removeClass('i-checkbox-unchecked').addClass('i-checkbox-checked');
+            }else{
+                $(this).removeClass('i-checkbox-checked').addClass('i-checkbox-unchecked');
+            }
+        });
+    }
 };
